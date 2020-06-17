ff2a28d73cb98ae134a64c67e250c5470ed04434
Eric Sanchez <esanchez@imgix.com>
2020-06-17 14:09:35 -0700

    feat: create signed and unsigned URLs

--- imgix.go
+++ imgix.go
@@ -11,116 +11,154 @@ import (
 	"unicode/utf8"
 )
 
-// Matches http:// and https://
-var RegexpHTTPAndS = regexp.MustCompile("https?://")
-
-// Regexp for all characters we should escape in a URI passed in.
-var RegexUrlCharactersToEscape = regexp.MustCompile("([^ a-zA-Z0-9_.-])")
-
-// Create a new Client with the given domain, with HTTPS enabled.
-func NewClient(domain string) Client {
-	return Client{domain: domain, secure: true}
+// Builder facilitates the building of URLs.
+type Builder struct {
+	domain   string
+	token    string
+	useHTTPS bool
 }
 
-// Create a new Client with the given domain and token. HTTPS enabled.
-func NewClientWithToken(domain string, token string) Client {
-	return Client{domain: domain, secure: true, token: token}
+// NewBuilder creates a new Builder with the given domain, with HTTPS enabled.
+func NewBuilder(domain string) Builder {
+	return Builder{domain: domain, useHTTPS: true}
 }
 
-// The Client is used to build URLs.
-type Client struct {
-	domain string
-	token  string
-	secure bool
+// NewBuilderWithToken creates a new Builder with the given domain and token
+// with HTTPS enabled.
+func NewBuilderWithToken(domain string, token string) Builder {
+	return Builder{domain: domain, useHTTPS: true, token: token}
 }
 
-// Returns whether HTTPS should be used.
-func (c *Client) Secure() bool {
-	return c.secure
+// UseHTTPS returns whether HTTPS or HTTP should be used.
+func (b *Builder) UseHTTPS() bool {
+	return b.useHTTPS
 }
 
-// Returns the URL scheme to use. One of 'http' or 'https'.
-func (c *Client) Scheme() string {
-	if c.Secure() {
+// SetUseHTTPS sets a builder's useHTTPS field to true or false. Setting
+// useHTTPS to false forces the builder to use HTTP.
+func (b *Builder) SetUseHTTPS(useHTTPS bool) {
+	b.useHTTPS = useHTTPS
+}
+
+// Scheme gets the URL scheme to use, either "http" or "https"
+// (the scheme uses HTTPS by default).
+func (b *Builder) Scheme() string {
+	if b.UseHTTPS() {
 		return "https"
 	} else {
 		return "http"
 	}
 }
 
-// Returns the domain for the given path.
-func (c *Client) Domain() string {
-	return RegexpHTTPAndS.ReplaceAllString(c.domain, "") // Strips out the scheme if exists
+// TODO: Review this regex-replace-all code.
+// Domain gets the builder's domain string.
+func (b *Builder) Domain() string {
+	return RegexpHTTPAndS.ReplaceAllString(b.domain, "") // Strips out the scheme if exists
 }
 
-// Creates and returns the URL signature in the form of "s=SIGNATURE" with
-// no values.
-func (c *Client) SignatureForPath(path string) string {
-	return c.SignatureForPathAndParams(path, url.Values{})
+// SetToken sets the token for this builder. This value will be used to sign
+// URLs created through the builder.
+func (b *Builder) SetToken(token string) {
+	b.token = token
 }
 
-// Creates and returns the URL signature in the form of "s=SIGNATURE" for
-// the given parameters. Requires that the client have a token.
-func (c *Client) SignatureForPathAndParams(path string, params url.Values) string {
-	if c.token == "" {
-		return ""
-	}
-
-	hasher := md5.New()
-	hasher.Write([]byte(c.token + path))
-
-	// Do not mix in the parameters into the signature hash if no parameters
-	// have been given
-	if len(params) != 0 {
-		hasher.Write([]byte("?" + params.Encode()))
-	}
-
-	return "s=" + hex.EncodeToString(hasher.Sum(nil))
+// CreateURL creates a URL string given a path and a set of
+// params.
+func (b *Builder) CreateURL(path string, params url.Values) string {
+	return b.createAndMaybeSignURL(path, params, false)
 }
 
-// Builds the full URL to the image (including the domain) with no params.
-func (c *Client) Path(imgPath string) string {
-	return c.PathWithParams(imgPath, url.Values{})
+// CreateSignedURL is like CreateURL except that it creates a signed URL.
+func (b *Builder) CreateSignedURL(path string, params url.Values) string {
+	return b.createAndMaybeSignURL(path, params, true)
 }
 
-// `PathWithParams` will manually build a URL from a given path string and
+// CreateURLFromPath creates a URL string given a path.
+func (b *Builder) CreateURLFromPath(path string) string {
+	return b.createAndMaybeSignURL(path, url.Values{}, false)
+}
+
+// CreateSignedURLFromPath is like CreateURLFromPath except that it creates
+// a full URL to the image that has been signed using the builder's token.
+func (b *Builder) CreateSignedURLFromPath(path string) string {
+	return b.createAndMaybeSignURL(path, url.Values{}, true)
+}
+
+// createURLFromPathAndParams will manually build a URL from a given path string and
 // parameters passed in. Because of the differences in how net/url escapes
 // path components, we need to manually build a URL as best we can.
-//
-// The behavior of this function is highly dependent upon its test suite.
-func (c *Client) PathWithParams(imgPath string, params url.Values) string {
+func (b *Builder) createAndMaybeSignURL(path string, params url.Values, shouldSign bool) string {
 	u := url.URL{
-		Scheme: c.Scheme(),
-		Host:   c.Domain(),
+		Scheme: b.Scheme(),
+		Host:   b.Domain(),
 	}
-
 	urlString := u.String()
 
+	// TODO: Review this cgiEscape code.
 	// If we are given a fully-qualified URL, escape it per the note located
-	// near the `cgiEscape` function definition
-	if RegexpHTTPAndS.MatchString(imgPath) {
-		imgPath = cgiEscape(imgPath)
+	// near the `cgiEscape` function definition.
+	if RegexpHTTPAndS.MatchString(path) {
+		path = cgiEscape(path)
+	}
+	// TODO: This portion is still a little busy...
+	path = maybePrependSlash(path)
+	urlWithPath := strings.Join([]string{urlString, path}, "")
+	maybeBase64EncodeParameters(&params)
+
+	var parameterString string
+	if shouldSign {
+		signature := b.signPathAndParams(path, params)
+		parameterString = createParameterString(params, signature)
+	} else {
+		parameterString = createParameterString(params, "")
 	}
 
-	// Add a leading slash if one does not exist:
-	//     "users/1.png" -> "/users/1.png"
-	if strings.Index(imgPath, "/") != 0 {
-		imgPath = "/" + imgPath
+	if parameterString != "" {
+		return strings.Join([]string{urlWithPath, parameterString}, "?")
 	}
+	return urlWithPath
+}
 
-	urlString += imgPath
+// signPathAndParams takes a builder's token value, a path, and a set of
+// params and creates a signature from these values.
+func (b *Builder) signPathAndParams(path string, params url.Values) string {
+	hasQueryParams := false
 
-	for key, val := range params {
+	if len(params) > 0 {
+		hasQueryParams = true
+	}
+	queryParams := params.Encode()
+	signature := createMd5Signature(b.token, path, queryParams)
+
+	if hasQueryParams {
+		return strings.Join([]string{"s=", signature}, "")
+	}
+	return strings.Join([]string{"s=", signature}, "")
+}
+
+// createMd5Signature creates the signature by joining the token, path, and params
+// strings into a signatureBase. Next, create a hashedSig and write the
+// signatureBase into it. Finally, return the encoded, signed string.
+func createMd5Signature(token string, path string, params string) string {
+	signatureBase := strings.Join([]string{token, path, params}, "")
+	hashedSig := md5.New()
+	hashedSig.Write([]byte(signatureBase))
+	return hex.EncodeToString(hashedSig.Sum(nil))
+}
+
+// maybeBase64EncodeParameters base64-encodes a parameter
+// if the parameter has the "64" suffix.
+func maybeBase64EncodeParameters(params *url.Values) {
+	for key, val := range *params {
 		if strings.HasSuffix(key, "64") {
 			encodedParam := base64EncodeParameter(val[0])
 			params.Set(key, encodedParam)
 		}
 	}
+}
 
-	// The signature in an imgix URL must always be the **last** parameter in a URL,
-	// hence some of the gross string concatenation here. net/url will aggressively
-	// alphabetize the URL parameters.
-	signature := c.SignatureForPathAndParams(imgPath, params)
+// TODO: Revisit this encoding and replacement code.
+func createParameterString(params url.Values, signature string) string {
 	parameterString := params.Encode()
 	parameterString = strings.Replace(parameterString, "+", "%%20", -1)
 
@@ -129,13 +167,18 @@ func (c *Client) PathWithParams(imgPath string, params url.Values) string {
 	} else if signature != "" && len(params) == 0 {
 		parameterString = signature
 	}
+	return parameterString
+}
 
-	// Only append the parameter string if it is not blank.
-	if parameterString != "" {
-		urlString += "?" + parameterString
+// maybePrependSlash prepends if the path does not begin with one:
+// "users/1.png" -> "/users/1.png"
+func maybePrependSlash(path string) string {
+	const Slash = "/"
+
+	if strings.Index(path, Slash) != 0 {
+		path = strings.Join([]string{Slash, path}, "")
 	}
-
-	return urlString
+	return path
 }
 
 // Base64-encodes a parameter according to imgix's Base64 variant requirements.
@@ -148,27 +191,21 @@ func base64EncodeParameter(param string) string {
 	return base64EncodedParam
 }
 
+// Todo: Review this regex and the one that follows.
+// Matches http:// and https://
+var RegexpHTTPAndS = regexp.MustCompile("https?://")
+
+// Regexp for all characters we should escape in a URI passed in.
+var RegexUrlCharactersToEscape = regexp.MustCompile("([^ a-zA-Z0-9_.-])")
+
 // This code is less than ideal, but it's the only way we've found out how to do it
 // give Go's URL capabilities and escaping behavior.
 //
-// This method replicates the beavhior of Ruby's CGI::escape in Go.
-//
-// Here is that method:
-//
-//     def CGI::escape(string)
-//       string.gsub(/([^ a-zA-Z0-9_.-]+)/) do
-//         '%' + $1.unpack('H2' * $1.bytesize).join('%').upcase
-//       end.tr(' ', '+')
-//      end
-//
-// It replaces
-//
-// See:
-//  - https://github.com/parkr/imgix-go/pull/1#issuecomment-109014369
-//  - https://github.com/imgix/imgix-blueprint#securing-urls
+// See: https://github.com/parkr/imgix-go/pull/1#issuecomment-109014369 and
+// https://github.com/imgix/imgix-blueprint#securing-urls
 func cgiEscape(s string) string {
 	return RegexUrlCharactersToEscape.ReplaceAllStringFunc(s, func(s string) string {
-		rune, _ := utf8.DecodeLastRuneInString(s)
-		return "%" + strings.ToUpper(fmt.Sprintf("%x", rune))
+		runeValue, _ := utf8.DecodeLastRuneInString(s)
+		return "%" + strings.ToUpper(fmt.Sprintf("%x", runeValue))
 	})
 }
--- imgix_test.go
+++ imgix_test.go
@@ -7,101 +7,102 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func testClient() Client {
-	return NewClient("prod.imgix.net")
+// TODO: Idiomatic testing.
+func testClient() Builder {
+	return NewBuilder("prod.imgix.net")
 }
 
-func testClientWithToken() Client {
-	return NewClientWithToken("my-social-network.imgix.net", "FOO123bar")
+func testClientWithToken() Builder {
+	return NewBuilderWithToken("my-social-network.imgix.net", "FOO123bar")
 }
 
 func TestBasicClientPath(t *testing.T) {
 	c := testClient()
-	assert.Equal(t, "https://prod.imgix.net/1/users.jpg", c.Path("/1/users.jpg"))
+	assert.Equal(t, "https://prod.imgix.net/1/users.jpg", c.CreateURLFromPath("/1/users.jpg"))
 }
 
 func TestClientPathWithParams(t *testing.T) {
 	c := testClient()
 	params := url.Values{"w": []string{"200"}, "h": []string{"400"}}
-	assert.Equal(t, "https://prod.imgix.net/1/users.jpg?h=400&w=200", c.PathWithParams("/1/users.jpg", params))
+	assert.Equal(t, "https://prod.imgix.net/1/users.jpg?h=400&w=200", c.CreateURL("/1/users.jpg", params))
 }
 
 func TestClientScheme(t *testing.T) {
 	c := testClient()
-	c.secure = false
+	c.useHTTPS = false
 	assert.Equal(t, "http", c.Scheme())
-	c.secure = true
+	c.useHTTPS = true
 	assert.Equal(t, "https", c.Scheme())
 }
 
 func TestClientPath(t *testing.T) {
 	c := testClient()
-	u := c.Path("/jax.jpg")
+	u := c.CreateURLFromPath("/jax.jpg")
 	assert.Equal(t, "https://prod.imgix.net/jax.jpg", u)
 }
 
 func TestClientPathWithOneParam(t *testing.T) {
 	c := testClient()
 	params := url.Values{"w": []string{"400"}}
-	u := c.PathWithParams("/jax.jpg", params)
+	u := c.CreateURL("/jax.jpg", params)
 	assert.Equal(t, "https://prod.imgix.net/jax.jpg?w=400", u)
 }
 
 func TestClientPathWithTwoParams(t *testing.T) {
 	c := testClient()
 	params := url.Values{"w": []string{"400"}, "h": []string{"300"}}
-	u := c.PathWithParams("/jax.jpg", params)
+	u := c.CreateURL("/jax.jpg", params)
 	assert.Equal(t, "https://prod.imgix.net/jax.jpg?h=300&w=400", u)
 }
 
 func TestClientPathWithParamsEncodesParamKeys(t *testing.T) {
 	c := testClient()
 	params := url.Values{"hello world": []string{"interesting"}}
-	u := c.PathWithParams("/demo.png", params)
+	u := c.CreateURL("/demo.png", params)
 	assert.Equal(t, "https://prod.imgix.net/demo.png?hello%%20world=interesting", u)
 }
 
 func TestClientPathWithParamsEncodesParamValues(t *testing.T) {
 	c := testClient()
 	params := url.Values{"hello_world": []string{"/foo\"> <script>alert(\"hacked\")</script><"}}
-	u := c.PathWithParams("/demo.png", params)
+	u := c.CreateURL("/demo.png", params)
 	assert.Equal(t, "https://prod.imgix.net/demo.png?hello_world=%2Ffoo%22%3E%%20%3Cscript%3Ealert%28%22hacked%22%29%3C%2Fscript%3E%3C", u)
 }
 
 func TestClientPathWithParamsEncodesBase64ParamVariants(t *testing.T) {
 	c := testClient()
 	params := url.Values{"txt64": []string{"I cannøt belîév∑ it wors! 😱"}}
-	u := c.PathWithParams("~text", params)
+	u := c.CreateURL("~text", params)
 	assert.Equal(t, "https://prod.imgix.net/~text?txt64=SSBjYW5uw7h0IGJlbMOuw6l24oiRIGl0IHdvcu-jv3MhIPCfmLE", u)
 }
 
 func TestClientPathWithSignature(t *testing.T) {
 	c := testClientWithToken()
-	u := c.Path("/users/1.png")
+	u := c.CreateSignedURLFromPath("/users/1.png")
 	assert.Equal(t, "https://my-social-network.imgix.net/users/1.png?s=6797c24146142d5b40bde3141fd3600c", u)
 }
 
 func TestClientPathWithSignatureAndParams(t *testing.T) {
 	c := testClientWithToken()
 	params := url.Values{"w": []string{"400"}, "h": []string{"300"}}
-	assert.Equal(t, "https://my-social-network.imgix.net/users/1.png?h=300&w=400&s=1a4e48641614d1109c6a7af51be23d18", c.PathWithParams("/users/1.png", params))
+	assert.Equal(t, "https://my-social-network.imgix.net/users/1.png?h=300&w=400&s=9de08f728192f92b7132176a0a17ef08", c.CreateSignedURL("/users/1.png", params))
 }
 
 func TestClientPathWithSignatureAndEmptyParams(t *testing.T) {
 	c := testClientWithToken()
 	params := url.Values{}
-	assert.Equal(t, "https://my-social-network.imgix.net/users/1.png?s=6797c24146142d5b40bde3141fd3600c", c.PathWithParams("/users/1.png", params))
+	assert.Equal(t, "https://my-social-network.imgix.net/users/1.png?s=6797c24146142d5b40bde3141fd3600c", c.CreateSignedURL("/users/1.png", params))
 }
 
 func TestClientFullyQualifiedUrlPath(t *testing.T) {
 	c := testClientWithToken()
-	assert.Equal(t, "https://my-social-network.imgix.net/http%3A%2F%2Favatars.com%2Fjohn-smith.png?s=493a52f008c91416351f8b33d4883135", c.Path("http://avatars.com/john-smith.png"))
+	assert.Equal(t, "https://my-social-network.imgix.net/http%3A%2F%2Favatars.com%2Fjohn-smith.png?s=493a52f008c91416351f8b33d4883135", c.CreateSignedURLFromPath("http://avatars.com/john-smith.png"))
 }
 
 func TestClientFullyQualifiedUrlPathWithParams(t *testing.T) {
 	c := testClientWithToken()
 	params := url.Values{"w": []string{"400"}, "h": []string{"300"}}
-	assert.Equal(t, "https://my-social-network.imgix.net/http%3A%2F%2Favatars.com%2Fjohn-smith.png?h=300&w=400&s=a201fe1a3caef4944dcb40f6ce99e746", c.PathWithParams("http://avatars.com/john-smith.png", params))
+	assert.Equal(t, "https://my-social-network.imgix.net/http%3A%2F%2Favatars.com%2Fjohn-smith.png?h=300&w=400&s=a58d87a5dfa5f7478e06715571e96f78", c.CreateSignedURL("http://avatars.com/john-smith.png", params))
 }
 
 func TestClientHostsCountValidation(t *testing.T) {
