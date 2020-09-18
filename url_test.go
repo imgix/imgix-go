d8ff91e2c96c4e15915307585de04ba9358e8192
Eric Sanchez <esanchez@imgix.com>
2020-09-18 08:52:08 -0700

    refactor: use ixparams over url.Values

--- url_test.go
+++ url_test.go
@@ -1,7 +1,6 @@
 package imgix
 
 import (
-	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -28,14 +27,15 @@ func testBuilder() URLBuilder {
 
 func TestURL_BasicPathNoParams(t *testing.T) {
 	u := testBuilder()
-	actual := u.CreateURL("image.png", url.Values{})
+	actual := u.CreateURL("image.png")
 	expected := "https://test.imgix.net/image.png"
 	assert.Equal(t, expected, actual)
 }
 
 func TestURL_BasicPathWithParams(t *testing.T) {
 	u := testBuilder()
-	actual := u.CreateURL("image.png", url.Values{"w": []string{"100"}})
+
+	actual := u.CreateURL("image.png", Param("w", "100"))
 	expected := "https://test.imgix.net/image.png?w=100"
 	assert.Equal(t, expected, actual)
 }
@@ -43,9 +43,8 @@ func TestURL_BasicPathWithParams(t *testing.T) {
 func TestURL_paramValuesAreEscaped(t *testing.T) {
 	key := "hello_world"
 	value := "/foo\"> <script>alert(\"hacked\")</script><"
-	params := url.Values{key: []string{value}}
 	u := testBuilder()
-	actual := u.CreateURL("image.png", params)
+	actual := u.CreateURL("image.png", Param(key, value))
 	expected := "https://test.imgix.net/image.png?hello_world=%2Ffoo%22%3E+%3Cscript%3Ealert%28%22hacked%22%29%3C%2Fscript%3E%3C"
 	assert.Equal(t, expected, actual)
 }
@@ -54,23 +53,21 @@ func TestURL_PathsArePlusSafe(t *testing.T) {
 	// https://github.com/imgix/imgix-core-js/issues/88
 	u := testBuilder()
 	expected := "https://test.imgix.net/E%2BP-003_D.jpeg"
-	actual := u.CreateURL("E+P-003_D.jpeg", url.Values{})
+	actual := u.CreateURL("E+P-003_D.jpeg")
 	assert.Equal(t, expected, actual)
 }
 
 func TestURL_Base64WithUnicodeParam(t *testing.T) {
 	u := testBuilder()
-	params := url.Values{"txt64": []string{"I cannøt belîév∑ it wors! 😱"}}
-	actual := u.CreateURL("~text", params)
+	actual := u.CreateURL("~text", Param("txt64", "I cannøt belîév∑ it wors! 😱"))
 	expected := "https://test.imgix.net/~text?txt64=SSBjYW5uw7h0IGJlbMOuw6l24oiRIGl0IHdvcu-jv3MhIPCfmLE"
 	assert.Equal(t, expected, actual)
 }
 
 func TestURL_WithRepeatedParamValues(t *testing.T) {
 	u := testBuilder()
-	params := url.Values{"auto": []string{"format", "compress"}}
 	expected := "https://test.imgix.net?auto=format%2Ccompress"
-	actual := u.CreateURL("", params)
+	actual := u.CreateURL("", Param("auto", "format", "compress"))
 	assert.Equal(t, expected, actual)
 }
 
@@ -78,7 +75,7 @@ func TestURL_BluePrintSigning(t *testing.T) {
 	u := NewURLBuilder("my-social-network.imgix.net", WithToken("FOO123bar"))
 	u.SetUseLibParam(false)
 	expected := "https://my-social-network.imgix.net/http%3A%2F%2Favatars.com%2Fjohn-smith.png?s=493a52f008c91416351f8b33d4883135"
-	actual := u.CreateURL("/http%3A%2F%2Favatars.com%2Fjohn-smith.png", url.Values{})
+	actual := u.CreateURL("/http%3A%2F%2Favatars.com%2Fjohn-smith.png")
 	assert.Equal(t, expected, actual)
 }
 
@@ -89,14 +86,13 @@ func TestURL_BluePrintSigningWithParams(t *testing.T) {
 		WithLibParam(false))
 
 	expected := "https://my-social-network.imgix.net/users/1.png?h=300&w=400&s=1a4e48641614d1109c6a7af51be23d18"
-	params := url.Values{"h": []string{"300"}, "w": []string{"400"}}
-
-	actualPathPrefixed := u.CreateURL("/users/1.png", params)
+	params := []IxParam{Param("h", "300"), Param("w", "400")}
+	actualPathPrefixed := u.CreateURL("/users/1.png", params...)
 	assert.Equal(t, expected, actualPathPrefixed)
 
 	// The only difference between this and the above is that
 	// the below is not prefixed with a slash.
-	actual := u.CreateURL("users/1.png", params)
+	actual := u.CreateURL("users/1.png", params...)
 	assert.Equal(t, expected, actual)
 }
 
@@ -105,8 +101,8 @@ func TestURL_BluePrintSigningWithProblematicParams(t *testing.T) {
 	u := testBuilder()
 	expected := "https://test.imgix.net/image.png?mark64=aHR0cHM6Ly9hc3NldHMuaW1naXgubmV0L2xvZ28ucG5n"
 
-	params := url.Values{"mark64": []string{"https://assets.imgix.net/logo.png"}}
-	actual := u.CreateURL("image.png", params)
+	params := []IxParam{Param("mark64", "https://assets.imgix.net/logo.png")}
+	actual := u.CreateURL("image.png", params...)
 	assert.Equal(t, expected, actual)
 }
 
@@ -117,7 +113,7 @@ func TestURL_SigningFullyQualifiedWithParams(t *testing.T) {
 		WithLibParam(false))
 	expected := "https://my-social-network.imgix.net/http%3A%2F%2Favatars.com%2Fjohn-smith.png?h=300&w=400&s=a201fe1a3caef4944dcb40f6ce99e746"
 
-	params := url.Values{"w": []string{"400"}, "h": []string{"300"}}
-	actual := u.CreateURL("/http%3A%2F%2Favatars.com%2Fjohn-smith.png", params)
+	params := []IxParam{Param("w", "400"), Param("h", "300")}
+	actual := u.CreateURL("/http%3A%2F%2Favatars.com%2Fjohn-smith.png", params...)
 	assert.Equal(t, expected, actual)
 }
