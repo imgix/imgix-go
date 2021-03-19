51d2516c6f1bc5d007816ac1ef9bc6cbcd8cddb0
Eric Sanchez <esanchez@imgix.com>
2021-03-19 14:41:35 -0700

    refactor(encoding_test): remove assert dependency

--- v2/encoding_test.go
+++ v2/encoding_test.go
@@ -3,89 +3,162 @@ package imgix
 import (
 	"encoding/base64"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestEncoding_isBase64(t *testing.T) {
-	assert.True(t, isBase64("64"))
-	assert.True(t, isBase64("   64"))
-	assert.True(t, isBase64("646464"))
-	assert.True(t, isBase64("fit64"))
-	assert.True(t, isBase64("markalign64"))
+	gotBase64 := isBase64("64")
+	if !gotBase64 {
+		t.Errorf("got:  %t; want: %t", gotBase64, true)
+	}
+
+	gotBase64 = isBase64("   64")
+	if !gotBase64 {
+		t.Errorf("got:  %t; want: %t", gotBase64, true)
+	}
+
+	gotBase64 = isBase64("646464")
+	if !gotBase64 {
+		t.Errorf("got:  %t; want: %t", gotBase64, true)
+	}
+
+	gotBase64 = isBase64("fit64")
+	if !gotBase64 {
+		t.Errorf("got:  %t; want: %t", gotBase64, true)
+	}
+	gotBase64 = isBase64("markalign64")
+	if !gotBase64 {
+		t.Errorf("got:  %t; want: %t", gotBase64, true)
+	}
 }
 
 func TestEncoding_isNotBase64(t *testing.T) {
-	assert.False(t, isBase64("6  4"))
-	assert.False(t, isBase64("646464 "))
-	assert.False(t, isBase64("\x40"))
+	// Ensure the following strings are NOT accepted as
+	// valid base64 keys.
+	gotBase64 := isBase64("6  4")
+	if gotBase64 {
+		t.Errorf("got:  %t; want: %t", gotBase64, true)
+	}
+
+	gotBase64 = isBase64("646464 ")
+	if gotBase64 {
+		t.Errorf("got:  %t; want: %t", gotBase64, true)
+	}
+
+	gotBase64 = isBase64("\x40")
+	if gotBase64 {
+		t.Errorf("got:  %t; want: %t", gotBase64, true)
+	}
 }
 
 func TestEncoding_base64EncodeQueryParamValue(t *testing.T) {
-	const expectedWarmUp = "SGVsbG8sIOS4lueVjA"
 	const data = "Hello, 世界"
-	actualWarmUp := base64EncodeQueryParamValue(data)
-	assert.Equal(t, expectedWarmUp, actualWarmUp)
+	const wantHello64 = "SGVsbG8sIOS4lueVjA"
+	gotHello64 := base64EncodeQueryParamValue(data)
 
-	const preEncoded = "Avenir Next Demi,Bold"
-	const expectedAve = "QXZlbmlyIE5leHQgRGVtaSxCb2xk"
-	actualAve := base64EncodeQueryParamValue("Avenir Next Demi,Bold")
-	assert.Equal(t, expectedAve, actualAve)
+	if gotHello64 != wantHello64 {
+		t.Errorf("got:  %s; want: %s", gotHello64, wantHello64)
+	}
 
-	decodedAve, _ := base64.StdEncoding.DecodeString(actualAve)
-	assert.Equal(t, preEncoded, string(decodedAve))
+	const wantAve64 = "QXZlbmlyIE5leHQgRGVtaSxCb2xk"
+	const preEncodedAveStr = "Avenir Next Demi,Bold"
+
+	gotAve64 := base64EncodeQueryParamValue(preEncodedAveStr)
+	if gotAve64 != wantAve64 {
+		t.Errorf("got:  %s; want: %s", gotAve64, wantAve64)
+	}
+
+	decodedAve, _ := base64.StdEncoding.DecodeString(gotAve64)
+
+	// Check that we can reconstruct the original string from
+	// the encoded string.
+	gotDecoded := string(decodedAve)
+	if gotDecoded != preEncodedAveStr {
+		t.Errorf("got:  %s; want: %s", gotDecoded, preEncodedAveStr)
+	}
 }
 
 func TestEncoding_base64UTF8(t *testing.T) {
 	s := `I cannøt belîév∑ it wors! 😱`
-	actual := base64EncodeQueryParamValue(s)
-	expected := "SSBjYW5uw7h0IGJlbMOuw6l24oiRIGl0IHdvcu-jv3MhIPCfmLE"
-	assert.Equal(t, expected, actual)
+	got := base64EncodeQueryParamValue(s)
+	want := "SSBjYW5uw7h0IGJlbMOuw6l24oiRIGl0IHdvcu-jv3MhIPCfmLE"
+
+	if got != want {
+		t.Errorf("\ngot:  %s\nwant: %s", got, want)
+	}
 }
 
 func TestEncoding_BlueprintBase64(t *testing.T) {
 	s := `Hello,+World!`
-	actual := base64EncodeQueryParamValue(s)
-	expected := "SGVsbG8sK1dvcmxkIQ"
-	assert.Equal(t, expected, actual)
+	got := base64EncodeQueryParamValue(s)
+	want := "SGVsbG8sK1dvcmxkIQ"
+
+	if got != want {
+		t.Errorf("\ngot:  %s\nwant: %s", got, want)
+	}
 }
 
 func TestEncoding_checkProxyStatusEmpty(t *testing.T) {
 	isProxy, isEncoded := checkProxyStatus("")
-	assert.Equal(t, false, isProxy)
-	assert.Equal(t, false, isEncoded)
+	const want = false
+
+	if isProxy || isEncoded {
+		t.Errorf("got:  isProxy == %t; want: isProxy == %t", isProxy, want)
+		t.Errorf("got:  isEncoded == %t; want: isEncoded == %t", isEncoded, want)
+	}
 }
 
 func TestEncoding_checkProxyStatusEncoded(t *testing.T) {
 	const encodedProxy = "http%3A%2F%2Fwww.this.com%2Fpic.jpg"
 	isProxy, isEncoded := checkProxyStatus(encodedProxy)
-	assert.Equal(t, true, isProxy)
-	assert.Equal(t, true, isEncoded)
+
+	const want = true
+	if !(isProxy && isEncoded) {
+		t.Errorf("got:  isProxy == %t; want: isProxy == %t", isProxy, want)
+		t.Errorf("got:  isEncoded == %t; want: isEncoded == %t", isEncoded, want)
+	}
 }
 
 func TestEncoding_checkProxyStatusAscii(t *testing.T) {
-
+	const wantProxy = true
+	const wantEncoded = false
 	const proxyHTTP = "http://www.this.com/pic.jpg"
 	isProxyHTTP, isEncodedHTTP := checkProxyStatus(proxyHTTP)
-	assert.Equal(t, true, isProxyHTTP)
-	assert.Equal(t, false, isEncodedHTTP)
+
+	if !isProxyHTTP {
+		t.Errorf("got:  isProxyHTTP == %t; want: isProxyHTTP == %t", isProxyHTTP, wantProxy)
+	}
+
+	if isEncodedHTTP {
+		t.Errorf("got:  isEncodedHTTP == %t; want: isEncodedHTTP == %t", isEncodedHTTP, wantEncoded)
+	}
 
 	const proxyHTTPS = "https://www.this.com/pic.jpg"
 	isProxyHTTPS, isEncodedHTTPS := checkProxyStatus(proxyHTTPS)
-	assert.Equal(t, true, isProxyHTTPS)
-	assert.Equal(t, false, isEncodedHTTPS)
+
+	if !isProxyHTTPS {
+		t.Errorf("got:  isProxyHTTPS == %t; want: isProxyHTTPS == %t", isProxyHTTPS, wantProxy)
+	}
+
+	if isEncodedHTTPS {
+		t.Errorf("got:  isEncodedHTTPS == %t; want: isEncodedHTTPS == %t", isEncodedHTTPS, wantEncoded)
+	}
 }
 
 func TestEncoding_encodePathProxyEncoded(t *testing.T) {
-	const expected = "/http%3A%2F%2Fwww.this.com%2Fpic.jpg"
-	actual := sanitizePath(expected)
-	assert.Equal(t, expected, actual)
+	const want = "/http%3A%2F%2Fwww.this.com%2Fpic.jpg"
+	got := sanitizePath(want)
+
+	if got != want {
+		t.Errorf("\ngot:  %s\nwant: %s", got, want)
+	}
 }
 
 func TestEncoding_encodePathProxyRaw(t *testing.T) {
 	const proxyPath = "http://www.this.com/pic.jpg"
-	const expected = "/http%3A%2F%2Fwww.this.com%2Fpic.jpg"
-	actual := sanitizePath(proxyPath)
+	const want = "/http%3A%2F%2Fwww.this.com%2Fpic.jpg"
+	got := sanitizePath(proxyPath)
 
-	assert.Equal(t, expected, actual)
+	if got != want {
+		t.Errorf("\ngot:  %s\nwant: %s", got, want)
+	}
 }
