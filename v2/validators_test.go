bed0cc6ff49d79407f7be594bf839c32e5e863dd
Eric Sanchez <esanchez@imgix.com>
2021-03-18 18:47:16 -0700

    refactor(validators_test): remove assert dependency

--- v2/validators_test.go
+++ v2/validators_test.go
@@ -2,55 +2,102 @@ package imgix
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestValidators_validateNegativeWidths(t *testing.T) {
 	widths := []int{100, 200, 300, -400, -500}
-	validWidths, err := validateWidths(widths)
+	got, err := validateWidths(widths)
 
-	// Ensure an error occurred, and the `err` is `NotEqual` to `nil`.
-	assert.NotEqual(t, nil, err)
-	assert.Equal(t, []int{}, validWidths)
+	// Assert an error occurred, i.e. that the `err` is NOT `nil`.
+	// If the err is nil, fail.
+	if err == nil {
+		t.Errorf("got: err == nil; want: err != nil")
+	}
+
+	want := []int{}
+
+	// Assert the lengths of the wanted widths and gotten widths
+	// are the same (i.e. that both are empty arrays).
+	if len(got) != len(want) {
+		t.Errorf("got: %v; want: %v", got, want)
+	}
 }
 
 func TestValidators_validatePositiveWidths(t *testing.T) {
-	expected := []int{101, 202, 303, 404, 505}
-	validWidths, err := validateWidths(expected)
+	want := []int{101, 202, 303, 404, 505}
+	got, err := validateWidths(want)
 
-	// Check the `err` is nil.
-	assert.Equal(t, nil, err)
-	// Check the expected widths are valid widths.
-	assert.Equal(t, expected, validWidths)
+	// Assert NO error occurred, i.e. err SHOULD BE nil.
+	// If not, fail.
+	if err != nil {
+		t.Errorf("got: err != nil; want: err == nil")
+	}
+
+	// We want the lengths to be equal, fail if they aren't.
+	if len(got) != len(want) {
+		t.Errorf("got: len(got) != len(want); want: len(got) == len(want)")
+	}
+
+	// Check that we got the widths we wanted. If not, fail and detail
+	// the indices at which the arrays differ.
+	for idx, v := range want {
+		if got[idx] != v {
+			t.Errorf("got: %v; want: %v; arrays differ at index %d", got[idx], v, idx)
+		}
+	}
 }
 
 func TestValidators_validateMinWidthValid(t *testing.T) {
-	const OneHundred = 100
-	validValue, err := validateMinWidth(OneHundred)
-	assert.Equal(t, OneHundred, validValue)
-	assert.Equal(t, nil, err)
+	const want = 100
+	got, err := validateMinWidth(want)
+
+	// Assert NO error occurred, i.e. err SHOULD BE nil.
+	// If not, fail.
+	if err != nil {
+		t.Errorf("got: err != nil; want: err == nil")
+	}
+
+	// We want 100, but if we got something else, FAIL!
+	if got != want {
+		t.Errorf("got: %v; want: %v", got, want)
+	}
 }
 
 func TestValidators_validateMinWidthInvalid(t *testing.T) {
 	const LessThanZero = -1
-	invalidValue, err := validateMinWidth(LessThanZero)
-	assert.Equal(t, -1, invalidValue)
-	assert.NotEqual(t, nil, err)
+	_, err := validateMinWidth(LessThanZero)
+
+	// Assert an error occurred, i.e. that the `err` is NOT `nil`.
+	// If the err is nil, fail.
+	if err == nil {
+		t.Errorf("got: err == nil; want: err != nil")
+	}
 }
 
 func TestValidators_validateMaxWidthValid(t *testing.T) {
-	const OneHundred = 100
-	validValue, err := validateMaxWidth(OneHundred)
-	assert.Equal(t, OneHundred, validValue)
-	assert.Equal(t, nil, err)
+	const want = 100
+	got, err := validateMaxWidth(want)
+
+	// Assert NO error occurred, i.e. err SHOULD BE nil.
+	// If not, fail.
+	if err != nil {
+		t.Errorf("got: err != nil; want: err == nil")
+	}
+
+	if got != want {
+		t.Errorf("got: %v; want: %v", got, want)
+	}
 }
 
 func TestValidators_validateMaxWidthInvalid(t *testing.T) {
 	const LessThanZero = -1
-	invalidValue, err := validateMaxWidth(LessThanZero)
-	assert.Equal(t, -1, invalidValue)
-	assert.NotEqual(t, nil, err)
+	_, err := validateMaxWidth(LessThanZero)
+
+	// Assert an error occurred, i.e. that the `err` is NOT `nil`.
+	// If the err is nil, fail.
+	if err == nil {
+		t.Errorf("got: err == nil; want: err != nil")
+	}
 }
 
 func TestValidators_validateRangeInvalid(t *testing.T) {
@@ -58,24 +105,51 @@ func TestValidators_validateRangeInvalid(t *testing.T) {
 	end := 320
 
 	_, err := validateRange(begin, end)
-	assert.NotEqual(t, nil, err)
+
+	// Assert an error occurred, i.e. that the `err` is NOT `nil`.
+	// If the err is nil, fail.
+	if err == nil {
+		t.Errorf("got: err == nil; want: err != nil")
+	}
 }
 
 func TestValidators_validateRangeValid(t *testing.T) {
-	rp := rangePair{minWidth: 100, maxWidth: 8192}
-	validRangePair, err := validateRange(rp.minWidth, rp.maxWidth)
-	assert.Equal(t, rp, validRangePair)
-	assert.Equal(t, nil, err)
+	want := rangePair{minWidth: 100, maxWidth: 8192}
+	got, err := validateRange(want.minWidth, want.maxWidth)
+
+	// Assert NO error occurred, i.e. err SHOULD BE nil.
+	// If not, fail.
+	if err != nil {
+		t.Errorf("got: err != nil; want: err == nil")
+	}
+
+	if got != want {
+		t.Errorf("got: %v; want: %v", got, want)
+	}
 }
 
 func TestValidators_validateRangeWithToleranceInvalid(t *testing.T) {
 	invalidTolerance := 0.001
 	_, err := validateRangeWithTolerance(100, 200, invalidTolerance)
-	assert.NotEqual(t, nil, err)
+
+	// Assert an error occurred, i.e. that the `err` is NOT `nil`.
+	// If the err is nil, fail.
+	if err == nil {
+		t.Errorf("got: err == nil; want: err != nil")
+	}
 }
 
 func TestValidators_validateRangeWithToleranceValid(t *testing.T) {
-	invalidTolerance := 1.25
-	_, err := validateRangeWithTolerance(100, 200, invalidTolerance)
-	assert.Equal(t, nil, err)
+	const want = 1.25
+	got, err := validateRangeWithTolerance(100, 200, want)
+
+	// Assert NO error occurred, i.e. err SHOULD BE nil.
+	// If not, fail.
+	if err != nil {
+		t.Errorf("got: err != nil; want: err == nil")
+	}
+
+	if got.tolerance != want {
+		t.Errorf("got: %v; want: %v", got.tolerance, want)
+	}
 }
